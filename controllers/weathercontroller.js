7347225ba065fa5a2ec409363476b589fa637521
usman ali <usmanali4073@gmail.com>
2019-12-15 21:46:09 -0500

    Promise Paralle calling

--- controllers/weathercontroller.js
+++ controllers/weathercontroller.js
@@ -2,18 +2,13 @@ const weatherservice = require('../services/weatherService');
 
 function weathercontroller() {
     async function get(req, res) {
-        let result = { country: 12, cite: null }
+        let result = { country: null, city: null };
 
-        result.country = await weatherservice().getWeatherCountry().then((data) => {
-            let country = {
-                weather: data.weather,
-                sys: data.sys
-            }
-            return country;
-        });
+        [result.country, result.city] = await Promise.all([
+            weatherservice().getWeatherCountry().then((data) => data),
+            weatherservice().getWeatherbyCite().then((data) => data)
+        ]);
 
-
-        result.cite = await weatherservice().getWeatherbyCite().then((data) => data);
         return res.send(result).status(200)
     }
     return {get }
